1521405c152d3e63bde9d3d9b580bbda7f99be55
Pavel <triodjangopiter@yandex.ru>
2016-09-19 22:34:50 +0300

    add stucture

---
+++ chapter_001/src/main/java/ru/pravvich/Calculate.java
@@ -0,0 +1,64 @@
+packege ru.pravvich;
+/**
+ * Implements calculator. Supports repeated using previous calculation.
+*/
+public class Calculate {
+
+	public static void main(String[] args) {
+	double result;
+	
+	}
+	
+
+        /**
+        *  The addition operations.
+	*/ @param first - first argument, second - second argument
+
+	public void add(double first, double second) {
+	this.resoult = first + second;
+	
+	}
+
+
+        /**
+        *  Substraction operation.
+	*/ @param first - first argument, second - second argument
+
+	public void substract(double first, double second) {
+	this.resoult = first - second;
+	
+	}
+
+
+        /**
+        *  Division operation.
+	*/ @param first - first argument, second - second argument
+
+	public void mult(double first, double second) {
+	this.resoult = first * second;
+	
+	}
+
+
+        /**
+        *  Division operation.
+	*/ @param first - first argument, second - second argument
+
+	public void div(double first, double second) {
+	
+	if (second != 0) {
+	this.resoult = first / second;
+	} else {
+		System.out.println("?елить на ноль нельз¤!");
+	}
+
+	this.resoult = first / second;
+	
+	}
+
+//The and...
+
+}
+
+
+
